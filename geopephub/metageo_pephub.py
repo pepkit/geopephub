20ab5cb774cf953142b39eefefba01a41383a712
Mirochill <Mirochill@users.noreply.github.com>
2026-05-21 19:49:25 +0200

    Use raw string for bedbase file filter

--- geopephub/metageo_pephub.py
+++ geopephub/metageo_pephub.py
@@ -195,7 +195,7 @@ def _upload_gse_project(
     """
     if target == "bedbase":
         geofetcher_obj = geofetch.Geofetcher(
-            filter="\.(bed|bigBed|narrowPeak|broadPeak)\.",
+            filter=r"\.(bed|bigBed|narrowPeak|broadPeak)\.",
             filter_size=BEDBASE_MAX_SIZE,
             data_source="all",
             processed=True,
